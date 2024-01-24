f57da8384a5f697862f2d88a8f96a6fa9814e0da
Loona <loona.mac@gmail.com>
2024-01-24 15:33:38 +0100

    fin ver 1.5, début ver 1.6: émission aux extrémités

--- lineFormation/lineFormation_v1.6/README.md
+++ lineFormation/lineFormation_v1.6/README.md
@@ -1,10 +1,5 @@
-# Line formation v1.5:
+# Line formation v1.6:
 ## Principe
 * placement d'un robot leader immobile qui envoie des signaux dans toutes les directions tous les ticks, (i.e toutes les deux secondes). Lorsqu'il reçoit une réponse d'un robot placé près de lui, il n'émet plus que dans l'axe de la ligne ainsi formée.
 * placement de followers un par un aléatoirement qui font un wander en continu (pas de tick). Ils interrogent leur environnement toutes les secondes pour savoir si il y a un signal. Si ils trouvent le leader/la ligne, ils s'immobilisent et se comportent comme le leader. + modification du jump set pour qu'il démarre toujours à full puis descende vers 3/4.
-
-## Problème
-Les robots ont tendance à croiser le signal d'un robot au milieu de la ligne et de s'immobiliser à cet endroit (à cause de la largeur de leur champ IR). Cela forme plus des clusters qu'une véritable ligne (qui n'est déjà pas très droite)
-
-## Idée
-Faire en sorte que seuls les robots se trouvant aux extrémités de la ligne émettent, seulement dans la direction de l'axe de la ligne mais dans le sens opposé. De cette façon, il n'y aura plus de signaux IR entre les robots au sein de la ligne
+* seuls les robots à l'extrémité de la ligne émettent un message, dans l'axe de la ligne
---
+++ lineFormation/lineFormation_v1.6/leader/main.c
@@ -0,0 +1,103 @@
+/**
+ * POGOBOT
+ *
+ * Copyright © 2022 Sorbonne Université ISIR
+ * This file is licensed under the Expat License, sometimes known as the MIT License.
+ * Please refer to file LICENCE for details.
+**/
+
+/* 
+Algorithm of line formation v1
+> behavior of the leader: staying still and sending signals
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "pogobot.h"
+
+
+#define message_length_bytes 6
+#define tick 1000000   // the leader will notify the environment every tick microseconds
+
+
+int main(void) {
+
+    // Initialize the Pogobot - MANDATORY
+    pogobot_init();
+    printf("init ok\n");
+    
+
+    ir_direction dir;   // direction to emit in
+    // signal to send as the leader
+    pogobot_infrared_set_power( pogobot_infrared_emitter_power_oneThird );
+    unsigned char message[] = "still";
+
+    time_reference_t t0;
+    uint32_t t1;
+    while (1){
+        pogobot_stopwatch_reset(&t0);
+
+        // listen to signals:
+        pogobot_infrared_update();
+        // if it hears a signal it tries to align with the emitter
+        if ( pogobot_infrared_message_available() ) {
+            message_t mr;
+            // get the next message in the buffer and store it in mr
+            pogobot_infrared_recover_next_message(&mr);
+                
+            // get the sender id of the message
+            int sender_id = mr.header._sender_id;  
+            // get the ir emitter of the sender
+            int ir_emitter_id = mr.header._sender_ir_index;
+            // get the ir I received it on
+            int ir_receiver_id = mr.header._receiver_ir_index;    
+            // get the payload of the message
+            unsigned char *payload = mr.payload;
+
+
+            // if someone is following the leader, it starts emitting in only one direction. Else it emits in all directions
+            if (strcmp(payload, "still") == 0) {
+                
+                pogobot_led_setColor(0,255,0);
+                switch (ir_receiver_id)
+                {
+                    case 0: // front => emit back
+                        printf("signal received on IR captor front\n");
+                        dir = 2;
+                        break;
+                    case 1: // right => emit left
+                        printf("signal received on IR captor right\n");
+                        dir = 3;
+                        break;
+                    case 2: // back => emit front
+                        printf("signal received on IR captor back\n");
+                        dir = 0;
+                        break;
+                    case 3: // left => emit right
+                        printf("signal received on IR captor left\n");
+                        dir = 1;
+                        break;
+                }
+
+                pogobot_infrared_sendMessageOneDirection(dir, 0x1234, message, message_length_bytes);
+            } else {
+                pogobot_led_setColor(255,0,0);
+                // send a message in every direction, with the id of the origin ir emitter
+                pogobot_infrared_sendMessageAllDirectionWithId(0x1234, message, message_length_bytes);
+                //printf("I am here!\n");
+            }
+        } else {
+            pogobot_led_setColor(255,0,0);
+            // send a message in every direction, with the id of the origin ir emitter
+            pogobot_infrared_sendMessageAllDirectionWithId(0x1234, message, message_length_bytes);
+            //printf("I am here!\n");
+        }
+
+        pogobot_led_setColor(0,0,0);
+
+        t1 = pogobot_stopwatch_get_elapsed_microseconds(&t0);
+        msleep((tick - t1)/1000);   // msleep in milliseconds
+    }
+
+}
